14d558d3fccfd12a5f6ca7a31a346cd8de53af9e
BK <qwerty0911@gachon.ac.kr>
2022-12-25 00:11:12 +0900

    권한 추가

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,17 +5,8 @@ const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const {campgroundSchema, reviewSchema} = require('../schemas')
 const Review = require('../models/review')
-const {isLoggedIn} = require('../middleware')
-
-const validateCampground = (req,res,next)=>{
-    const {error} = campgroundSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400);
-    } else{
-        next();
-    }
-}
+const { find } = require('../models/review');
+const {isLoggedIn,  isAuthor, validateCampground} =  require('../middleware')
 
 router.get('/', async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -31,6 +22,7 @@ router.get('/new',isLoggedIn , (req, res) => {
 //post 생성 요청
 router.post('/',validateCampground,isLoggedIn, catchAsync( async (req, res, next) => {
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id
     await campground.save();
     req.flash('success','Successfully made a  new campground!')
     res.redirect(`/campgrounds/${campground._id}`);
@@ -38,7 +30,12 @@ router.post('/',validateCampground,isLoggedIn, catchAsync( async (req, res, next
 
 //post 상세 페이지 랜더
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate({
+        path:'reviews',
+        populate:{
+            path:'author'
+        }
+    }).populate('author');
     if(!campground){
         req.flash('error','Cannot find that campground...')
         res.redirect('/campgrounds')
@@ -47,17 +44,19 @@ router.get('/:id', catchAsync(async (req, res) => {
 }))
 
 //post 수정 form
-router.get('/:id/edit',isLoggedIn, async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+router.get('/:id/edit',isLoggedIn,isAuthor, async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
+
     if(!campground){
         req.flash('error','Cannot find that campground...')
         res.redirect('/campgrounds')
-    }
+    }//페이지가 없을때
     res.render('campgrounds/edit', { campground })
 })
 
 //post 업데이트 요청
-router.put('/:id', validateCampground,isLoggedIn, catchAsync( async (req, res) => {
+router.put('/:id', validateCampground,isLoggedIn,isAuthor, catchAsync( async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
     req.flash('success','Successfully update a campground!')
@@ -65,7 +64,7 @@ router.put('/:id', validateCampground,isLoggedIn, catchAsync( async (req, res) =
 }))
 
 //post 삭제
-router.delete('/:id',isLoggedIn, async (req, res) => {
+router.delete('/:id',isLoggedIn, isAuthor, async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndDelete(id)
     req.flash('success','Successfully delete a campground!')
